9632add0490e55f6907e49d4e62d54489881a8ca
alhammanazil <alhammanazil18@gmail.com>
2024-01-16 11:26:53 +0700

    learn reactive data in vue.js

---
+++ Practice/Chapter 1/script.js
@@ -0,0 +1,12 @@
+var product = {
+    "name" : "Bamboo Thermal Ski Coat",
+    "description" : "The ultimate in high-performance insulated outerwear, our Thermal Ski Coat is crafted with innovative PrimaLoft® ThermoPlume insulation that retains its loft and warmth even when wet.",
+    "price" : 179,
+    "image" : "https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg",
+    "quantity" : 1
+}
+
+var app = new Vue({
+    el: '#app',
+    data: product
+});
