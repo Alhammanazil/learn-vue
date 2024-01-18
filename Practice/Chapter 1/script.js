ea0b7eeccfb7d6140168409125b8199ce3ba4ebb
alhammanazil <alhammanazil18@gmail.com>
2024-01-18 15:52:36 +0700

    toggle UI with computed property and transition effect

--- Practice/Chapter 1/script.js
+++ Practice/Chapter 1/script.js
@@ -1,21 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
-        maximum: 100,
-        products: null,
-        cart: [],
         style: {
             label: ['font-weight-bold', 'mr-2'],
-            inputWidth: 70,
-            sliderStatus: true
-        }
+            inputWidth: 60,
+            sliderStatus: false,
+        },
+        maximum: 100,
+        products: null,
+        cart: []
     },
     mounted: function() {
         fetch('https://hplussport.com/api/products/order/price')
-        .then(response => response.json())
-        .then(data => {
-            this.products = data;
-        });
+            .then(response => response.json())
+            .then(data => {
+                this.products = data;
+            });
     },
     computed: {
         sliderState: function() {
@@ -23,6 +23,21 @@ var app = new Vue({
         }
     },
     methods: {
+        before: function (el) {
+            el.className = 'd-none'
+        },
+        enter: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animated fadeInRight'
+            }, delay)
+        },
+        leave: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animated fadeOutRight'
+            }, delay)
+        },
         addItem: function(product) {
             this.cart.push(product);
         }
