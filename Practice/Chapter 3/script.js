c68380b85049f5c9f3cc5670f992b0038375d7c9
alhammanazil <alhammanazil18@gmail.com>
2024-01-18 14:22:32 +0700

    Computed Property

---
+++ Practice/Chapter 3/script.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        slugText: 'The Quick #(*!&@ Brown 29'
+    },
+    computed: {
+        slugetize: function () {
+            return this.slugText
+                .toLowerCase()
+                .replace(/[^\w ]+/g, '')
+                .replace(/ +/g, '-') + '-' + this.now();
+        }
+    },
+    methods: {
+        now: function () {
+            return new Date().getTime();
+        }
+    }
+});
