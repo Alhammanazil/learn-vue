45690b44fc91e7dd51296ab9f53286f478eb05cf
alhammanazil <alhammanazil18@gmail.com>
2024-01-18 16:19:41 +0700

    fix transition effect

--- Practice/Chapter 3/script.js
+++ Practice/Chapter 3/script.js
@@ -1,34 +1,45 @@
 var app = new Vue({
     el: '#app',
     data: {
-        slugText: 'The Quick #(*!&@ Brown 29'
+        style: {
+            label: ['font-weight-bold', 'mr-2'],
+            inputWidth: 60,
+            sliderStatus: false,
+        },
+        maximum: 100,
+        products: null,
+        cart: []
+    },
+    mounted: function() {
+        fetch('https://hplussport.com/api/products/order/price')
+            .then(response => response.json())
+            .then(data => {
+                this.products = data;
+            });
     },
     computed: {
-        // now: function () {
-        //     var date = new Date();
-        //     return (
-        //         String(date.getHours()) + 
-        //         String(date.getMinutes()) +
-        //         String(date.getSeconds())
-        //     )
-        // },
- 
-        slugetize: function () {
-            return this.slugText
-                .toLowerCase()
-                .replace(/[^\w ]+/g, '')
-                .replace(/ +/g, '-') + '-' + this.now();
+        sliderState: function() {
+            return this.style.sliderStatus ? 'd-flex' : 'd-none';
         }
     },
-
     methods: {
-        now: function () {
-            var date = new Date();
-            return (
-                String(date.getHours()) + 
-                String(date.getMinutes()) +
-                String(date.getSeconds())
-            )
+        before: function (el) {
+            el.className = 'd-none'
         },
+        enter: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animated fadeInRight'
+            }, delay)
+        },
+        leave: function (el) {
+            var delay = el.dataset.index * 100;
+            setTimeout(function() {
+                el.className = 'row d-flex mb-3 align-items-center animated fadeOutRight'
+            }, delay)
+        },
+        addItem: function(product) {
+            this.cart.push(product);
+        }
     }
 });
