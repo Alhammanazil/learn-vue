7fbe3dc2e8940c337d92cda4cefa34a2b4f04bf2
alhammanazil <alhammanazil18@gmail.com>
2024-01-22 13:51:49 +0700

    Categorizing Array Data

--- Practice/Chapter 3/script.js
+++ Practice/Chapter 3/script.js
@@ -1,7 +1,3 @@
-Vue.filter('currencyFormat', function(value) {
-    return Number.parseFloat(value).toFixed(2);
-});
-
 var app = new Vue({
     el: '#app',
     data: {
@@ -10,7 +6,7 @@ var app = new Vue({
             inputWidth: 60,
             sliderStatus: false,
         },
-        maximum: 100,
+        maximum: 50,
         products: null,
         cart: []
     },
@@ -22,33 +18,61 @@ var app = new Vue({
             });
     },
     filters: {
-        currencyFormat: function(value) {
-            return Number.parseFloat(value).toFixed(2);
+        currencyFormat: function (value) {
+            return '$' + Number.parseFloat(value).toFixed(2);
         }
     },
     computed: {
         sliderState: function() {
             return this.style.sliderStatus ? 'd-flex' : 'd-none';
+        },
+        cartTotal: function () {
+            let sum = 0;
+            for (key in this.cart) {
+                sum = sum + (this.cart[key].product.price * this.cart[key].qty);
+            }
+            return sum;
+        },
+        cartQty: function () {
+            let qty = 0;
+            for (key in this.cart) {
+                qty = qty + this.cart[key].qty;
+            }
+            return qty;
         }
     },
     methods: {
-        before: function(el) {
+        before: function (el) {
             el.className = 'd-none'
         },
-        enter: function(el) {
+        enter: function (el) {
             var delay = el.dataset.index * 100;
             setTimeout(function() {
                 el.className = 'row d-flex mb-3 align-items-center animated fadeInRight'
             }, delay)
         },
-        leave: function(el) {
+        leave: function (el) {
             var delay = el.dataset.index * 100;
             setTimeout(function() {
                 el.className = 'row d-flex mb-3 align-items-center animated fadeOutRight'
             }, delay)
         },
         addItem: function(product) {
-            this.cart.push(product);
+            var productIndex;
+            var productExist = this.cart.filter(function(item, index) {
+                if (item.product.id == Number(product.id)) {
+                    productIndex = index;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            if (productExist.length) {
+                this.cart[productIndex].qty++
+            } else {
+                this.cart.push({product: product, qty: 1});
+            }
         }
     }
 });
